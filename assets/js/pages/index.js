661ec4bbaa619e68b5dbd3a6cec7409d5d8e119d
Anthony.M <marcellin.anthony77@gmail.com>
2022-10-08 02:26:50 +0200

    Intégration des spécificitées de la recherche

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -14,11 +14,55 @@ function mainSearch() {
     
     inputSearchBar.addEventListener( 'keyup', (event) => {
         const element = event.target;
-        if (element.value.length > 3 ) {
-            const recipesFiltered = recipes.filter( (recipe) => recipe.name.toLowerCase().includes(element.value.toLowerCase()) );
-            // console.log(recipesFiltered)
-            displayRecipes(recipesFiltered);
+        if ( element.value.length > 2 ) {
+            // Recherche dans le champ du titre
+            let recipesFiltered =
+                recipes.filter(
+                    (recipe) => {
+                        let value = element.value.toLowerCase();
+                        return recipe.name
+                            .toLowerCase()
+                            .includes(value)
+                    }
+                );
 
+            // Recherche dans le champ de la description
+            recipesFiltered =
+                recipes.filter(
+                    (recipe) => {
+                        let value = element.value.toLowerCase();
+                        return recipe.description
+                            .toLowerCase()
+                            .includes(value)
+                    }
+                );
+
+            // Recherche dans le tableau des ingrédients
+            recipesFiltered =
+                recipes.filter(
+                    (recipe) => {
+                        let value = element.value.toLowerCase();
+                        // console.log( recipe.ingredients.forEach(
+                        //     (ingredient) => {
+                        //         return ingredient.ingredient
+                        //             .toLowerCase()
+                        //             .includes(value)
+                        //     }
+                        // ));
+
+                        let ingredientsRecipe = recipe.ingredients;
+
+                        for (let i = 0; i < ingredientsRecipe.length; i++) {
+                            return (ingredientsRecipe[i].ingredient
+                                .toLowerCase()
+                                .includes(value));
+                        };
+                    }
+                );
+
+            displayRecipes(recipesFiltered);
+        } else {
+            displayRecipes(recipes);
         }
         
     });
